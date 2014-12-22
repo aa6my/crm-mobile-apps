5448473f00fa6af0ac0e61a4a827f57c19cb0365
azimfkhri <afkhris@gmail.com>
2014-12-22 17:58:30 +0800

    done add, edit delete. but datepicker not yet

--- www/js/controllers/quote.js
+++ www/js/controllers/quote.js
@@ -1,24 +1,126 @@
-var apps = angular.module('quoteModule', []);
-	
-	apps.controller('Quote', function($scope, $http, Settings, init, Auth) {
-    url = Settings.url + '/dataAll/type/quotes/format/json';
-        $http.get(url, Auth.doAuth(init.username, init.password)).
-        success(function(data) {
-          $scope.quotes = data.quotes;
-          console.log('Success', data.quotes);
-    // For JSON responses, resp.data contains the result
-      $scope.doRefresh = function() {
-        $http.get(url, Auth.doAuth(init.username, init.password))
-         .success(function(data) {
-           $scope.quotes = data.quotes;
-         })
-         .finally(function() {
-           // Stop the ion-refresher from spinning
-           $scope.$broadcast('scroll.refreshComplete');
-         });
-      };
-  }, function(err) {
-    console.error('ERR', err);
-    // err.status will contain the status code
-  })
-})
+var apps = angular.module('quoteModule', ['ionic']);
+    apps.controller('Quote',function($scope,$http, $state,$ionicPopup, Settings, init, Auth, UniversalFunction, CrudOperation) {
+       
+          /*=============== quote(initial start of page will call this part) ============================= */
+        
+        /*-------------- initial value for page to show or hide button in quote form add/edit-------------*/
+        var m = UniversalFunction.returnButtonOnly();
+        $scope.btnAdd = m.add;
+        $scope.btnEdit = m.edit;
+        /*---------------------------*/
+
+        /*------------initial value for form data of update function ----*/
+        $scope.formData = UniversalFunction.returnDisplayFormData();
+        /*---------------------------------------------------------------*/
+        
+         var url = Settings.url + '/dataAll/type/quotes/format/json';
+              $http
+                .get(url, Auth.doAuth(init.username, init.password))
+                .success(function(data){
+                    $scope.quotes = data.quotes;
+                
+                    $scope.doRefresh = function(){
+                      $http
+                        .get(url, Auth.doAuth(init.username, init.password))
+                        .success(function(data){
+                            $scope.quotes = data.quotes;
+                        })
+                        .finally(function(){
+                            $scope.$broadcast('scroll.refreshComplete');
+                       });
+                    };
+              }, function(err) {
+                  console.error('ERR', err);
+              
+              })
+
+               
+              $scope.goToAddDataPage = function(){
+
+                   $state.go('app.quoteAdd_Edit',{},{reload:false});
+                   /*------------- If click add new button show only submit button with save function--------------*/
+                   var m = UniversalFunction.buttonOnly(true,false);
+                   $scope.btnAdd = m.add;
+                   $scope.btnEdit = m.edit;
+                   /*---------------------------*/
+                   /*---- set form value to blank */
+                   UniversalFunction.displayFormData('');
+                   
+                  
+              }
+
+               $scope.goToEditDataPage = function(quotes){
+
+                    $state.go('app.quoteAdd_Edit',{},{reload:false});
+                    /*-------------If click edit button show only save button with edit function--------------*/
+                    var n           = UniversalFunction.buttonOnly(false,true);
+                    $scope.btnAdd   = n.add;
+                    $scope.btnEdit  = n.edit;
+                    /*---------------------------*/
+                    /*-- display value form list into update form */
+                    var b           = UniversalFunction.displayFormData(quotes);
+                    $scope.formData = b;
+                    
+              }
+
+          
+ 
+          /*================================ Add function ================================*/
+                $scope.addData  = function(){
+
+                    $scope.formData = {};                           // store data from form into formData onject
+                    var params      = '/dataAll';                   // request Api link
+                    var data        = {                             // data sent to Api
+                                        type : "quotes", 
+                                        formData : this.formData
+                        };
+                    var stateToRedirect = 'app.quotes';
+                    CrudOperation.add(params, data, stateToRedirect);  
+
+                } 
+        /*================================ End Add function ================================*/
+
+
+
+
+        /*================================ Edit function ================================*/
+                $scope.editData = function(){
+
+                    var params     = '/dataAll';                  // request Api link
+                    var dataUpdate = {                             // field column need to update
+                                        quote_subject : $scope.formData.quote_subject,
+                                        quote_date_created : $scope.formData.quote_date_created,
+                                        quote_valid_until : $scope.formData.quote_valid_until,
+                                        quote_discount : $scope.formData.quote_discount,
+                                        quote_customer_notes : $scope.formData.quote_customer_notes,
+                                        quote_status : $scope.formData.quote_status
+
+
+                        };
+                    var data       = {                             // data sent to Api
+                                      type : "quotes",
+                                      primaryKey : 'quote_id', 
+                                      primaryKeyVal : $scope.formData.quote_id,
+                                      formData : dataUpdate
+                        };
+                    var stateToRedirect = 'app.quotes';           // State that will redirect after update process success
+                    CrudOperation.update(params, data, stateToRedirect);  
+                } 
+        /*================================ End Edit function ================================*/
+
+
+
+
+        /*================================ Delete function ================================*/
+                $scope.deleteData = function(quote) {
+                    var params = '/dataAll/type/quotes/key/quote_id/val/'+quote.quote_id;
+                    CrudOperation.delete(params);
+                }
+          /*================================ End Delete function ================================*/
+
+
+
+
+
+      })
+
