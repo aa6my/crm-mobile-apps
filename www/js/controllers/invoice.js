23316e94bcbb59fe1895b1c0116512521d80b703
aa6my <aa6my@qq.com>
2015-01-27 11:57:38 +0800

    add copyright header

--- www/js/controllers/invoice.js
+++ www/js/controllers/invoice.js
@@ -1,3 +1,25 @@
+/**
+
+  +-+-+-+-+ +-+-+-+-+-+
+  |S|E|G|I| |M|i|D|a|e|
+  +-+-+-+-+ +-+-+-+-+-+
+
+ * CRM MOBILE APPLICATION
+ *
+ * http://www.segimidae.net
+ *
+ * Ionic Framework
+ * 
+ * @category   controllers
+ * @package    invoice.js
+ * @author     Nizam <nizam@segimidae.net>
+ * @author     Norlihazmey <norlihazmey@segimidae.net>
+ * @author     Azim <azim@segimidae.net>
+ * @license    SeGi MiDae
+ * @copyright  2015 SEGI MiDae
+ * @version    0.5.1
+*/
+
 var apps = angular.module('invoiceModule', ['ionic','ui.bootstrap']);
     apps.controller('Invoice',function($scope,$http, $state,$ionicPopup,$ionicModal, $stateParams, Settings, init, Auth, UniversalFunction, CrudOperation) {
        
