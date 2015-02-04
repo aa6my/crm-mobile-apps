9bb90bf5ac1c654af63a5093e791994b5b641057
metallurgical <norlihazmey89@gmail.com>
2015-02-04 17:24:32 +0800

    add ninify for providers in gulpjs file and add script for production and development use in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -41,14 +41,25 @@ gulp.task('jshint', function() {
 
 var src = './www/js/controllers/*.js';
 var dist = './www/js/controllers/dist'
-// JS concat, strip debugging and minify
-gulp.task('uglify', function() {
+
+gulp.task('uglify_controllers', function() {
   gulp.src(src)    
     .pipe(stripDebug())
     .pipe(uglify())
     .pipe(concat('script.js'))
     .pipe(gulp.dest(dist));
 });
+
+
+//var src = '[./www/js/providers/*.js, ]';
+/*var dist = './www/js/providers/dist';
+gulp.task('uglify_providers', function() {
+  gulp.src(['./www/js/providers/*.js','!./www/js/providers/angular-base64.js','!./www/js/providers/angular-resource.js','!./www/js/providers/angular-resource.min.js','!./www/js/providers/ui-bootstrap-tpls-0.12.0.js','!./www/js/providers/ui-bootstrap-tpls-0.12.0.min.js'])    
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(concat('script_providers.js'))
+    .pipe(gulp.dest(dist));
+});*/
 /*----------------------------------------*/
 
 
