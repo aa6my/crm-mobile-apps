893591eca19e63902a8bc2d73a86b5460ccc6831
azimfakhri <afkhris@gmail.com>
2015-02-04 14:49:02 +0800

    Add signout function and remove files from sidemenu

---
+++ www/js/controllers/signout.js
@@ -0,0 +1,20 @@
+var apps_signout = angular.module('signoutModule', []);
+	
+apps_signout.controller('Signout',['$scope','$http','$stateParams', 'Settings', '$state', 'init', 'Auth','$ionicPopup',function($scope,$http,$stateParams, Settings, $state, init, Auth,$ionicPopup) {
+  
+    $scope.doSignout = function(){
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Sign Out',
+            template: 'Are you sure you want to sign out?'
+        })
+        confirmPopup.then(function(res) {
+            if(res) {
+                init.username = "";
+                init.password = "";
+                $state.go('app.server');                
+            } else {
+                console.log('You are not sure');
+            }
+        })      
+    }  
+}])
